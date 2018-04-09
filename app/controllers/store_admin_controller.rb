35aa6c9ec7404d08b0c4eb9c6bd5a33a27be6841
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-09 23:58:51 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,4 +1,12 @@
 class StoreAdminController < ApplicationController
-  def admin
+
+  layout "admin"
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def invoice
+    render :layout => false
   end
 end
